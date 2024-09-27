b87454a678d4d2c48862b94df64aef88df73f937
Raed-Khwayreh <raedkh.2000@example.com>
2024-09-27 18:00:04 +0300

    fix: delete unused features

--- react-version/src/mocks/buttonsList.js
+++ react-version/src/mocks/buttonsList.js
@@ -1,6 +1,6 @@
 export const buttonsList = [
-  { title: "All", active: true },
-  { title: "Electronics", active: false },
-  { title: "Clothing", active: false },
-  { title: "Furniture", active: false },
+  { title: "All" },
+  { title: "Electronics" },
+  { title: "Clothing" },
+  { title: "Furniture" },
 ];
